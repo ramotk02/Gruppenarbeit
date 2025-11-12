157bbc6d431bee29aba07825c1e416cfe3d85ceb
Omar taky <omartaky.ot@gmail.com>
2025-11-12 13:46:03 +0100

    up

---
+++ script.js
@@ -0,0 +1,19 @@
+// Sélectionne tous les éléments à animer
+const animateElements = document.querySelectorAll('.animate-on-scroll');
+
+const observerOptions = {
+    threshold: 0.1
+};
+
+const animateObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('visible');
+            observer.unobserve(entry.target);
+        }
+    });
+}, observerOptions);
+
+animateElements.forEach(el => {
+    animateObserver.observe(el);
+});
